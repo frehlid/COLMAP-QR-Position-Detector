871841ecf1c0d760ab4a9a2b00a54c2629447627
Dieter Frehlich <frehlichd@gmail.com>
2025-03-03 13:47:32 -0800

    support for multiple QR codes

--- getQRPosition.py
+++ getQRPosition.py
@@ -25,15 +25,20 @@ def find_QR_corners(image_path):
 
     QRDetector = cv2.QRCodeDetector()
 
-    success, points = QRDetector.detect(img)
+    success, decoded_info, points, _ = QRDetector.detectAndDecodeMulti(img)
     if not success or points is None:
         return None
 
-    print(f"Found QR code in {image_path}")
+    print(f"Found QR code(s) in {image_path}")
 
-    # reshape to (4, 2)
-    points = np.squeeze(points).astype(np.float32)
-    return points
+    qr_results = []
+    for qr_string, qr_corners in zip(decoded_info, points):
+        if qr_string:
+            # reshape to (4,2)
+            qr_corners = np.squeeze(qr_corners).astype(np.float32)
+            qr_results.append((qr_string, qr_corners))
+
+    return qr_results
 
 
 def residual(x, observations):
@@ -82,8 +87,7 @@ def main():
     reconstruction = load_colmap_data()
     print("Loaded COLMAP data")
 
-    # store a bunch of observations of each corner for 
-    corner_observations = {i: [] for i in range(4)}
+    qr_observations = {}
 
     for img_id, img_info in reconstruction.images.items():
         img_path = f"{IMAGE_FOLDER}/{img_info.name}"
@@ -91,8 +95,8 @@ def main():
         if (not os.path.exists(img_path)):
             continue
 
-        corners = find_QR_corners(img_path)
-        if corners is None:
+        qr_codes = find_QR_corners(img_path)
+        if qr_codes is None:
             continue 
 
 
@@ -105,78 +109,88 @@ def main():
         if (not img_info.has_pose):
             print(f"Skipping image {img_info.name} because there is no valid pose")
             continue
+        
+        for qr_string, corners in qr_codes:
+            if qr_string not in qr_observations:
+                qr_observations[qr_string] = {i: [] for i in range(4)}
 
-        for corner_index, pixel in enumerate(corners):
-            u,v = pixel
-            p_homogenous = np.array([u, v, 1.0], dtype=np.float64)
+            for corner_index, pixel in enumerate(corners):
+                u,v = pixel
+                p_homogenous = np.array([u, v, 1.0], dtype=np.float64)
 
-            # see here https://github.com/colmap/colmap/issues/1596
-            R = cam_from_world.rotation.matrix()
-            t = cam_from_world.translation
-            ray_dir = R.T @ (Kinv @ p_homogenous)  
-            ray_origin = - R.T @ t  
+                # see here https://github.com/colmap/colmap/issues/1596
+                R = cam_from_world.rotation.matrix()
+                t = cam_from_world.translation
+                ray_dir = R.T @ (Kinv @ p_homogenous)  
+                ray_origin = - R.T @ t  
 
-           # print(f"ray dir: {ray_dir}, ray_origin: {ray_origin}")
+               # print(f"ray dir: {ray_dir}, ray_origin: {ray_origin}")
 
-            corner_observations[corner_index].append({
-                "camera":camera,
-                "img_info": img_info,
-                "pixel":pixel,
-                "ray_origin": ray_origin,
-                "ray_dir": ray_dir,
-                })
+                corner_observations[corner_index].append({
+                    "camera":camera,
+                    "img_info": img_info,
+                    "pixel":pixel,
+                    "ray_origin": ray_origin,
+                    "ray_dir": ray_dir,
+                    })
 
-    corner_positions = {}
-    for corner_index in range(4):
-        observations = corner_observations[corner_index]
-        if len(observations) < 2:
-            ## TODO should I throw an error here?
-            print(f"Corner {corner_index} does not have enough observations...")
-            return 
+    qr_positions = {}
 
-        # super simple ransac -- TODO: use scikitlearn RANSAC??
-        # basically just trying to remove any outlier observations before least squares is run on inlier set
-        best_inliers = []
-        best_err = float('inf')
+    for qr_string, corners, in qr_observations.items():
+        corner_positions = {}
 
-        for _ in range(RANSAC_ITERS):
-            sample = np.random.choice(observations, size=2, replace=False)
-            pos = triangulate_corner(sample)
-    
-            errors = []
-            for obs in observations:
-                proj = obs["img_info"].project_point(pos)
-                if (proj is None): 
-                    errors.append(9999999.0)
-                else:
-                    errors.append(np.linalg.norm(proj - obs["pixel"]))
+        for corner_index in range(4):
+            observations = corner_observations[corner_index]
+            if len(observations) < 2:
+                ## TODO should I throw an error here?
+                print(f"Corner {corner_index} does not have enough observations...")
+                return 
 
-            inliers = [obs for obs,err in zip(observations, errors) if err < THRESH]
+            # super simple ransac -- TODO: use scikitlearn RANSAC??
+            # basically just trying to remove any outlier observations before least squares is run on inlier set
+            best_inliers = []
+            best_err = float('inf')
 
-            if len(inliers) > len(best_inliers):
-                best_inliers = inliers
+            for _ in range(RANSAC_ITERS):
+                sample = np.random.choice(observations, size=2, replace=False)
+                pos = triangulate_corner(sample)
+        
+                errors = []
+                for obs in observations:
+                    proj = obs["img_info"].project_point(pos)
+                    if (proj is None): 
+                        errors.append(9999999.0)
+                    else:
+                        errors.append(np.linalg.norm(proj - obs["pixel"]))
 
-        if len(best_inliers) > 0:
-            print(f"Triangulated corner {corner_index} with {len(best_inliers)} inliers")
-            corner_positions[corner_index] = triangulate_corner(best_inliers)
-        else:
-            print(f"Failed to triangulate corner {corner_index}")
+                inliers = [obs for obs,err in zip(observations, errors) if err < THRESH]
 
-    for index, pos in corner_positions.items():
-        print(f"Triangulated corner {index}: {pos}")
+                if len(inliers) > len(best_inliers):
+                    best_inliers = inliers
 
-    top_dist = np.linalg.norm(corner_positions[1] - corner_positions[0])
-    scale = QR_CODE_REAL_SIZE_M / top_dist;
-    print(f"Real size / calculated size: {scale}")
+            if len(best_inliers) > 0:
+                print(f"Triangulated corner {corner_index} with {len(best_inliers)} inliers")
+                corner_positions[corner_index] = triangulate_corner(best_inliers)
+            else:
+                print(f"Failed to triangulate corner {corner_index}")
 
+    for qr_string, corners in qr_positions.items():
+        print(f"Triangulated corners for QR code: {qr_string}")
 
-    gs_up = corner_positions[0] - corner_positions[1]
-    gs_up = gs_up / np.linalg.norm(gs_up)
-    gs_up *= -1
+        for index, pos in corners.items():
+            print(f"Corner {index}: {pos}")
 
-    gs_floor = corner_positions[0] + (QR_CODE_REAL_SIZE_M / scale) * gs_up
+        top_dist = np.linalg.norm(corner[1] - corner[0])
+        scale = QR_CODE_REAL_SIZE_M / top_dist;
+        print(f"Real size / calculated size: {scale}")
 
-    print(f"Y position of floor {y_floor_gs}")
+        gs_up = corner[0] - corner[3]
+        gs_up = gs_up / np.linalg.norm(gs_up)
+        gs_up *= -1
+
+        gs_floor = corner[0] + (QR_CODE_REAL_SIZE_M / scale) * gs_up
+
+        print(f"Position of floor {gs_floor}")
 
 
 if __name__ == "__main__":
